2575921c06cf0e5c6da60dfb27edfde838e82784
John Gutierrez <djterez88@gmail.com>
2021-02-25 17:40:34 -0800

    Setting up modrelates project

--- modrelates/modsr/views.py
+++ modrelates/modsr/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    hello = "Hello World"
+    context = {'hello': hello}
+
+    return render(request, "modsr/index.html", context)
